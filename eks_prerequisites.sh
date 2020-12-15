0c3a54f3008c80d34e637a2022ff0912b1c93a58
siljabg <30825783+siljabg@users.noreply.github.com>
2020-12-15 11:48:00 +0100

    Update eks_prerequisites.sh

--- eks_prerequisites.sh
+++ eks_prerequisites.sh
@@ -16,21 +16,21 @@ else
     echo "Please install docker" >&2
 fi
 
-#OFFICIAL AWS EKS USERGUIDE
-#https://docs.aws.amazon.com/eks/latest/userguide/getting-started-eksctl.html
+##OFFICIAL AWS EKS USERGUIDE
+##https://docs.aws.amazon.com/eks/latest/userguide/getting-started-eksctl.html
 
-#install aws cli v2
+##install aws cli v2
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 aws --version
 
-#install eksctl
+##install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 
-#install kubectl
-#kubectl version can be 2 version below master version
+##install kubectl
+##kubectl version can be 2 version below master version
 curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl
 curl -o kubectl.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.18.9/2020-11-02/bin/linux/amd64/kubectl.sha256
 openssl sha1 -sha256 kubectl
@@ -38,12 +38,25 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin
 kubectl version --short --client
 
-#install helm
+##install helm
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-#Configure AWS CLI credentials
+##Configure AWS CLI credentials
 aws configure set aws_access_key_id AKIASIZY5IJBXMAW2OFP
 aws configure set aws_secret_access_key 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
 aws configure set default.region us-west-2
+
+##Python 3 for centos
+#yum install -y python3
+# or to install latest version
+#yum install gcc openssl-devel bzip2-devel libffi-devel -y
+#curl -O https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz
+#tar -xzf Python-3.9.1.tgz
+#cd Python-3.9.1/
+#./configure --enable-optimizations
+#make altinstall
+#python3.9
+##check whether pip is installed
+#python -m pip --version
